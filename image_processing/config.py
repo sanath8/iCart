eb91a110ddd8d9363ab56482ca42b1537dcf4ae4
Sammed Ravanavar <33900745+sammedravanavar@users.noreply.github.com>
2019-03-09 16:06:57 +0530

    refactoring done

---
+++ image_processing/config.py
@@ -0,0 +1,4 @@
+#here are all the config information
+class config:
+    environment = "development"
+    image_file_name = ""
--- image_processing/main.py
+++ image_processing/main.py
@@ -2,20 +2,24 @@ from PhotoToBase64API import PhotoToBase64API
 from UploadAPI import UploadAPI
 from FacialAttributesAPI import FacialAttributesAPI
 from APIBank import APIBank
-
-file_name = ""
-
-api_bank = APIBank()
-photoToBase64API = PhotoToBase64API('/home/puneeth/Documents/Projects/iCart/images/boy.jpeg')
-uploadAPI = UploadAPI()
-facialAttributesAPI = FacialAttributesAPI()
-uploadAPI.getAPIInfo(api_bank)
-facialAttributesAPI.getAPIInfo(api_bank)
+from config import config
 
 
+class MainClass:
+    def run(self):
+        api_bank = APIBank()
+        if config.environment == "development":
+            photoToBase64API = PhotoToBase64API('/home/puneeth/Documents/Projects/iCart/images/boy.jpeg')
+            uploadAPI = UploadAPI()
+            facialAttributesAPI = FacialAttributesAPI()
+            uploadAPI.getAPIInfo(api_bank)
+            facialAttributesAPI.getAPIInfo(api_bank)
 
-base64_string = photoToBase64API.convertToBase64()
-uploadAPI.setImageBase64(base64_string)
-img_uid = uploadAPI.run()
-facialAttributesAPI.setImgUid(img_uid)
-facialAttributesAPI.run()
+
+        elif config.environment == "production":
+            photoToBase64API = PhotoToBase64API(config.image_file_name)
+            uploadAPI = UploadAPI()
+            facialAttributesAPI = FacialAttributesAPI()
+            uploadAPI.getAPIInfo(api_bank)
+            facialAttributesAPI.getAPIInfo(api_bank)
+
