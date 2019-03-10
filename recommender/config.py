c5f9a6dfa80abbceb135edc36b65aa3a05bc4d36
sanath8 <sanathbhargav26@gmail.com>
2019-03-11 01:36:09 +0530

    recommendation done

--- recommender/config.py
+++ recommender/config.py
@@ -1,4 +1,4 @@
 class config:
-    model_path = 'data_analytics/model_parameters.sav'
-    encoder_gender = 'data_analytics/classes_gender.npy'
-    encoder_shopping_month = 'data_analytics/classes_shopping_month.npy'
+    model_path = 'data_analysis/model_parameters.sav'
+    encoder_gender = 'data_analysis/classes_gender.npy'
+    encoder_shopping_month = 'data_analysis/classes_shopping_month.npy'
