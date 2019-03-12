c805ef060e228ca320eb379ff8dc863767be2cd3
sanath8 <sanathbhargav26@gmail.com>
2019-03-12 23:00:30 +0530

    cors added

--- recommender/recommendation_server.py
+++ recommender/recommendation_server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 import json
 from recommender import Recommender
-
+from flask_cors import CORS
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/')
 def index():
