31af07532939b068592182719cb31db1d7a36455
sanath8 <sanathbhargav26@gmail.com>
2019-03-15 12:16:21 +0530

    blue tooth sensing made modular

--- indoor_positioning/thread.py
+++ indoor_positioning/thread.py
@@ -3,6 +3,7 @@
 import threading
 import time
 from bt_proximity import BluetoothRSSI
+from mobile import MobileInfo
 exitFlag = 0
 
 class Nodes:
@@ -14,6 +15,9 @@ class Nodes:
 
 	def get_nearest_node(self):
 		return self.nearest_node
+
+	def get_nearest_node_name(self):
+		return MobileInfo.mobiles[self.nearest_node]["name"]
 	
 class myThread (threading.Thread):
 	def __init__(self, threadID, name, b_addr):
@@ -23,7 +27,7 @@ class myThread (threading.Thread):
 		self.b_addr = b_addr
 
 	def run(self):
-		IPS(self.b_addr).run_tracking_system(self.name, 20, 1)
+		IPS(self.b_addr).run_tracking_system(self.threadID, self.name, 20, 1)
 
 
 class IPS:
@@ -31,7 +35,7 @@ class IPS:
 		self.no_signal_counter = 0
 		self.b_addr = b_addr
 
-	def run_tracking_system(self, threadName, counter, delay):
+	def run_tracking_system(self, threadId, threadName, counter, delay):
 		b = BluetoothRSSI(addr=self.b_addr)
 		while True:
 			if exitFlag:
@@ -42,8 +46,8 @@ class IPS:
 			if(self.sensor_passive(rssi)):
 				b = self.reset_bluetooth_instance()
 			if(rssi > 0):
-				node_marker.set_nearest_node(threadName)
-			print("Nearest node is ", node_marker.get_nearest_node())
+				node_marker.set_nearest_node(threadId)
+			print("Nearest node-name is ", node_marker.get_nearest_node_name(), "Node ID", node_marker.get_nearest_node())
 			counter -= 1
 
 	def get_rssi_value(self, b):
@@ -51,7 +55,7 @@ class IPS:
 	   return rssi
 
 	def sensor_passive(self, rssi):
-		if(rssi == 0):
+		if(rssi == 0 or rssi == None):
 			self.no_signal_counter += 1
 		else:
 			self.no_signal_counter = 0
@@ -67,13 +71,20 @@ class IPS:
 node_marker = Nodes()
 
 # Create new threads
-addr1="74:23:44:3F:58:04"
-addr2="70:BB:E9:4A:94:17"
-thread1 = myThread(1, "note-3", addr1)
-thread2 = myThread(2, "note-6 pro", addr2)
+#addr1="74:23:44:3F:58:04"
+#addr2="70:BB:E9:4A:94:17"
+#thread1 = myThread(1, "note-3", addr1)
+#thread2 = myThread(2, "note-6 pro", addr2)
 
 # Start new Threads
-thread1.start()
-thread2.start()
+#thread1.start()
+#thread2.start()
+thread_list = []
+for each_key in MobileInfo.mobiles:
+	thread = myThread(each_key, MobileInfo.mobiles[each_key]["name"], MobileInfo.mobiles[each_key]["bt_addr"])
+	thread_list.append(thread)	
+	thread.start()
+
+
 
 print "Exiting Main Thread"
