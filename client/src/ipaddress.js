d6a63db8e8e7494899fbfbf026a2bbd9cece33cd
Sammed Ravanavar <33900745+sammedravanavar@users.noreply.github.com>
2019-05-11 12:56:09 +0530

    local ipadress

--- client/src/ipaddress.js
+++ client/src/ipaddress.js
@@ -1,8 +1,11 @@
 const ips = {
-    "recommendationUrl": "http://192.168.0.103",
-    "shortestPathUrl": "http://192.168.0.103",
-    "shopbotUrl": "http://192.168.0.103",
+    // "recommendationUrl": "http://192.168.0.103",
+    // "shortestPathUrl": "http://192.168.0.103",
+    // "shopbotUrl": "http://192.168.0.103",
     "raspberryPiUrl" : "http://192.168.0.100",
     "webSocketUrl" : "ws://192.168.0.100",
+    "recommendationUrl": "http://localhost",
+    "shortestPathUrl": "http://localhost",
+    "shopbotUrl": "http://localhost",
 }
 export default ips;
